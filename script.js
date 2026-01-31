80dac309aa384ea6b43a07ac2ea89156ea68cdf1
RG-hub6 <robbegermis@gmail.com>
2026-01-31 21:43:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Budgettool gestart");
+let currentIncome = 0;
+
+function addIncome() {
+  const incomeInput = document.getElementById("income");
+  const incomeValue = parseFloat(incomeInput.value);
+  if (!isNaN(incomeValue)) {
+    currentIncome += incomeValue;
+    document.getElementById("currentIncome").innerText = currentIncome.toFixed(2);
+    incomeInput.value = "";
+  } else {
+    alert("Voer een geldig bedrag in!");
+  }
+}
